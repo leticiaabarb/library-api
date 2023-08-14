b74b44b0d672b0a3f397693903a9cc309d7bd076
Leticia Barbosa <44868362+leticiaabarb@users.noreply.github.com>
2023-08-14 14:19:00 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose" 
 
-mongoose.connect("mongodb+srv://Leticiaabarb:Lele9898123@cluster0.f0kqbrl.mongodb.net/node-express");
+mongoose.connect("mongodb+srv://##########@cluster0.f0kqbrl.mongodb.net/node-express");
 
 let db = mongoose.connection;
 
